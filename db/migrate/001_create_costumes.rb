662b440780d17b4ea07e7bd5c8528f493f3d53c5
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-23 19:48:53 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,11 +1,12 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
     def change
         create_table :costume do |t|
-            t.string :name
-            t.float :price
-            t.text :image_url
-            t.string :size
-            t.timestamps
+          t.string "name"
+          t.integer "price"
+          t.string "size"
+          t.string "website"
+          t.datetime "created_at"
+          t.datetime "updated_at"
         end
     end
 end
