9e842776554100a902505b3cb1814aa2fcb669d6
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-23 20:21:07 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,4 @@ Bundler.require
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
-
-ENV["SINATRA_ENV"] ||= 'development'
-ActiveRecord::Base.establish_connection(ENV["SINATRA_ENV"].to_sym)
+ActiveRecord::Base.establish_connection(connection_details)
