065091a1ad1c3e08184362ce6889206ed52de2a8
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-23 19:38:36 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -9,4 +9,8 @@ Bundler.require
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
-ActiveRecord::Base.establish_connection(connection_details)
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
